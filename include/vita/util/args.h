80380849c505f36e81c769a718143f55548028f2
kirill.saidov <kirill.saidov.d@gmail.com>
2021-11-17 19:32:11 +0600

    args.h/.c relocated

---
+++ include/vita/util/args.h
@@ -0,0 +1,36 @@
+#ifndef VITA_ARGS_H
+#define VITA_ARGS_H
+
+/** VITA_ARGS MODULE
+	- getopt
+*/
+
+#include <stdarg.h>
+
+#include "../container/str.h"
+#include "../container/plist.h"
+
+typedef struct ArgOpt {
+	plist_t *cmdLong;
+	plist_t *cmdShort;
+	plist_t *cmdDesc;
+
+	str_t *helpManual;
+	bool helpWanted;
+} argopt_t;
+
+/**
+Parses command line arguments
+
+Params:
+	argc = number of arguments
+	argv = arguments
+	numOptions = number of command line options
+	... = option, description, variable where to save value
+
+Returns: `argopt_t*` upon sucess, `NULL` otherwise
+*/
+extern argopt_t *args_parse(const int argc, const char **const argv, const size_t numOptions, ...);
+extern void args_free(argopt_t *a);
+
+#endif // VITA_ARGS_H
