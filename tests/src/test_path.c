06afd18f20db3b2f62c13471568ad1d0ab2d6d0a
Kirill Saidov <kirillsaidovd@yandex.com>
2022-08-26 05:55:16 +0600

    testing path_build

--- tests/src/test_path.c
+++ tests/src/test_path.c
@@ -7,30 +7,30 @@
 void free_str(void *ptr, size_t i);
 
 int main(void) {
-	str_t *s = path_build(NULL, "hello", "world"); {
-		assert(str_equals(cstr(s), "hello/world"));
-	} str_free(s);
+    plist_t *p = plist_create(3); {
+        plist_push(p, "hello");
+        plist_push(p, "world");
+        plist_push(p, "folder");
 
-	plist_t *p = plist_create(3); {
-		plist_push(p, "hello");
-		plist_push(p, "world");
-		plist_push(p, "folder");
+        str_t *sp = path_build_n(NULL, p); {
+            assert(str_equals(cstr(sp), "hello/world/folder"));
+        } str_free(sp);
+    } plist_free(p);
 
-		str_t *sp = path_build_n(NULL, p); {
-			assert(str_equals(cstr(sp), "hello/world/folder"));
-		} str_free(sp);
-	} plist_free(p);
-
-	assert(path_exists("/home/lala") == false); // must fail
+    assert(path_exists("/home/lala") == false); // must fail
 
     #if defined(_WIN32) || defined(_WIN64)
+        str_t *s = path_build(NULL, "hello", "world", "media", "dev");
+        assert(str_equals(cstr(s), "hello\\world\\media\\dev\\"));
+        str_free(s);
+
         str_t *cwd = path_getcwd(); {
             assert(str_equals(cstr(cwd), "D:\\myfiles\\media\\dev\\repos\\gitlab.kirill.saidov\\vita\\tests"));
         } str_free(cwd);
 
         assert(path_exists("D:\\myfiles\\media\\dev\\repos\\gitlab.kirill.saidov\\vita\\tests"));
         assert(path_is_dir("D:\\myfiles\\media\\dev\\repos\\gitlab.kirill.saidov\\vita\\tests\\src"));
-    	assert(path_is_file("D:\\myfiles\\media\\dev\\repos\\gitlab.kirill.saidov\\vita\\tests\\src\\test_path.c"));
+        assert(path_is_file("D:\\myfiles\\media\\dev\\repos\\gitlab.kirill.saidov\\vita\\tests\\src\\test_path.c"));
 
         plist_t *pdir = path_listdir(NULL, "D:\\myfiles\\media\\dev\\repos\\gitlab.kirill.saidov\\vita\\tests\\src", true); {
             assert(plist_len(pdir) == FILES_IN_DIR);
@@ -45,13 +45,17 @@ int main(void) {
         // path_mkdir("hello_test_dir"); // works
         // path_mkdir_parents("\\hello\\world\\of\\my\\"); // works
     #elif defined(__linux__)
+        str_t *s = path_build(NULL, "hello", "world", "media", "dev");
+        assert(str_equals(cstr(s), "hello/world/media/dev/"));
+        str_free(s);
+
         str_t *cwd = path_getcwd(); {
             assert(str_equals(cstr(cwd), "/home/kirill/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src"));
         } str_free(cwd);
 
         assert(path_exists("/home/kirill/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src"));
         assert(path_is_dir("/home/kirill/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src"));
-    	assert(path_is_file("/home/kirill/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src/test_path.c"));
+        assert(path_is_file("/home/kirill/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src/test_path.c"));
 
         plist_t *pdir = path_listdir(NULL, "/home/kirill/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src/", true); {
             assert(plist_len(pdir) == FILES_IN_DIR);
@@ -66,13 +70,17 @@ int main(void) {
         // path_mkdir("hello_test_dir"); // works
         // path_mkdir_parents("/hello/world/of/my/"); // works
     #else
+        str_t *s = path_build(NULL, "hello", "world", "media", "dev");
+        assert(str_equals(cstr(s), "hello/world/media/dev/"));
+        str_free(s);
+
         str_t *cwd = path_getcwd(); {
             assert(str_equals(cstr(cwd), "/Users/KS/Desktop/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src"));
         } str_free(cwd);
 
         assert(path_exists("/Users/KS/Desktop/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src"));
         assert(path_is_dir("/Users/KS/Desktop/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src"));
-    	assert(path_is_file("/Users/KS/Desktop/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src/test_path.c"));
+        assert(path_is_file("/Users/KS/Desktop/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src/test_path.c"));
 
         plist_t *pdir = path_listdir(NULL, "/Users/KS/Desktop/myfiles/media/dev/repos/gitlab.kirill.saidov/vita/tests/src/", true); {
             assert(plist_len(pdir) == FILES_IN_DIR);
@@ -98,7 +106,7 @@ int main(void) {
 }
 
 void free_str(void *ptr, size_t i) {
-	str_free(ptr);
+    str_free(ptr);
 }
 
 
