2870388d9a83784aa6e896bc881858230f145fa8
Kirill Saidov <kirillsaidovd@yandex.com>
2022-09-08 16:04:19 +0600

    checking return value of arg_parse

--- tests/src/test_argopt.c
+++ tests/src/test_argopt.c
@@ -27,38 +27,56 @@ int32_t main(void) {
 
     // arguments
     const char *argv[] = {
-        "./test_argopt", 									// 1 +
-        "-r=../temp", "--wpath", "./docs/dw/data/",			// 3 +
-        "--intensity", "0.7", "-p=0.35", "--verbose", "-a", // 5 +
-        "-vl=83", "--level", "13", "--update", "y",         // 4 +
-        "-ug=y", "-uo", "--unkownOption", "unkownValue"		// 5 = 18
+        /*  progname: */"./test_argopt",                    // 1
+
+        /*     rpath: */ "-r=../temp",                      // 2
+        /*     wpath: */ "--wpath", "./docs/dw/data/",      // 4
+        
+        /* intensity: */ "--intensity=0.7",                 // 5
+        /*  polarity: */ "-p", "0.35",                      // 7
+       
+        /*   verbose: */ "--verbose",                       // 8
+        /*     audio: */ "-a", "true",                      // 10
+        
+        /*    volume: */ "-vl=83",                          // 11
+        /*     level: */ "--level", "13",                   // 13
+        
+        /*    update: */ "--update=y",                      // 14
+        /*   upgrade: */ "-ug", "y",                        // 16
+        
+        /*   unknown: */ "-uo",                             // 17
+        /*   unknown: */ "--unkownOption", "unkownValue",   // 19
+
+        // "-r", "../temp", "--wpath", "./docs/dw/data", "--intensity", "0.7"
     };
-    const size_t argc = 18;
+    const size_t argc = 19;
+    // const size_t argc = 7;
 
     // options
     argopt_t optv[] = {
-        { "--rpath", "-r", "read path", OPT(rpath), dt_str },
-        { "--wpath", "-w", "save path", OPT(wpath), dt_str },
+        { "--rpath", "-r", "read path", ARGOPT(rpath), dt_str },
+        { "--wpath", "-w", "save path", ARGOPT(wpath), dt_str },
 
-        { "--intensity", "-i", "level of intensity between [0; 1]", OPT(intensity), dt_float },
-        { "--polarity", "-p", "level of polarity between [0; 1]", OPT(polarity), dt_float },
+        { "--intensity", "-i", "level of intensity between [0; 1]", ARGOPT(intensity), dt_float },
+        { "--polarity", "-p", "level of polarity between [0; 1]", ARGOPT(polarity), dt_float },
 
-        { "--verbose", "-v", "verbose output", OPT(verbose), dt_bool },
-        { "--audio", "-a", "include audio", OPT(audio), dt_bool },
+        { "--verbose", "-v", "verbose output", ARGOPT(verbose), dt_bool },
+        { "--audio", "-a", "include audio", ARGOPT(audio), dt_bool },
 
-        { "--volume", "-vl", "volume level", OPT(volume), dt_int },
-        { "--level", "-l", "output level", OPT(level), dt_int },
+        { "--volume", "-vl", "volume level", ARGOPT(volume), dt_int },
+        { "--level", "-l", "output level", ARGOPT(level), dt_int },
 
-        { "--update", "-ud", "update (y, n)", OPT(update), dt_char },
-        { "--upgrade", "-ug", "upgrade (y, n)", OPT(upgrade), dt_char }
+        { "--update", "-ud", "update (y, n)", ARGOPT(update), dt_char },
+        { "--upgrade", "-ug", "upgrade (y, n)", ARGOPT(upgrade), dt_char }
     };
     const size_t optc = sizeof(optv)/sizeof(argopt_t);
 
     // parse args and opts
-    argopt_parse(argc, argv, optc, optv);
+    const int8_t parse_status = argopt_parse(argc, argv, optc, optv);
     
     // check
-    assert(str_equals(cstr(rpath), "../temp"));    
+    assert(parse_status == ARGOPT_PARSE_ERROR);
+    assert(str_equals(cstr(rpath), "../temp"));
     assert(str_equals(cstr(wpath), "./docs/dw/data/"));
     assert(intensity == (float)0.7);
     assert(polarity == (float)0.35);
