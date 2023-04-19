dc0a34015a5764b4a081f81b22b7bfbda5ff53c5
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-04-19 20:08:21 +0600

    window build script added

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+if not exist build ( 
+    md build 
+)
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\mingw64\bin\gcc.exe -H%cd% -B%cd%/build -G "MinGW Makefiles"
+cmake --build %cd%/build --config Debug
+
+
