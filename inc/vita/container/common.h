08fad14d889a0aa4f1d08fe8f4c7e584c0e6eb33
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-05-28 17:22:45 +0600

    moved common container related functionality from core/core.h to container/common.h

---
+++ inc/vita/container/common.h
@@ -0,0 +1,103 @@
+#ifndef VITA_COMMON_H
+#define VITA_COMMON_H
+
+/** VITA_COMMON MODULE
+    - vt_array_head
+    - vt_array_len
+    - vt_array_capacity
+    - vt_array_has_space
+    - vt_array_elsize
+    - vt_index_2d_to_1d
+    - vt_index_1d_to_2d
+*/
+
+#include "../core/core.h"
+#include "../util/debug.h"
+#include "../allocator/mallocator.h"
+
+/** Allocates memory for VitaBaseArrayType structure
+    @param alloctr allocator instance
+    @returns `VitaBaseArrayType*` upon success, `NULL` otherwise
+
+    @note if `NULL` is specified, then vita calloc/realloc/free is used
+*/
+extern struct VitaBaseArrayType *vt_array_new(const struct VitaBaseAllocatorType *const alloctr);
+
+/** Frees the VitaBaseArrayType instance
+    @param vbat VitaBaseArrayType pointer
+*/
+extern void vt_array_free(struct VitaBaseArrayType *vbat);
+
+/** Returns VitaBaseArrayType's ptr head
+    @param vbat VitaBaseArrayType ptr instance
+    @returns `NULL` upon failure
+*/
+extern void *vt_array_head(const struct VitaBaseArrayType *const vbat);
+
+/** Returns VitaBaseArrayType's length    
+    @param vbat VitaBaseArrayType ptr
+    @returns length
+*/
+extern size_t vt_array_len(const struct VitaBaseArrayType *const vbat);
+
+/** Returns VitaBaseArrayType's capacity
+    @param vbat VitaBaseArrayType ptr
+    @returns capacity
+*/
+extern size_t vt_array_capacity(const struct VitaBaseArrayType *const vbat);
+
+/** Returns available space before new allocation is required
+    @param vbat VitaBaseArrayType ptr
+    @returns free space (capacity - length)
+*/
+extern size_t vt_array_has_space(const struct VitaBaseArrayType *const vbat);
+
+/** Returns VitaBaseArrayType's element size
+    @param vbat VitaBaseArrayType ptr
+    @returns element size
+*/
+extern size_t vt_array_elsize(const struct VitaBaseArrayType *const vbat);
+
+/** Checks if VitaBaseArrayType has a valid allocator instance
+    @returns ditto
+*/
+extern bool vt_array_has_alloctr(const struct VitaBaseArrayType *const vbat);
+
+/** Slides through the container elements one by one
+    @param vbat VitaBaseArrayType ptr
+    @returns container ptr head pointing to next element from the start
+
+    @note returns `NULL` upon reaching the end
+*/
+extern void *vt_array_slide_front(struct VitaBaseArrayType *const vbat);
+
+/** Slides through the container elements one by one
+    @param vbat VitaBaseArrayType ptr
+    @returns container ptr head pointing to next element from the end
+
+    @note returns `NULL` upon reaching the end
+*/
+extern void *vt_array_slide_back(struct VitaBaseArrayType *const vbat);
+
+/** Resets the slider
+    @param vbat VitaBaseArrayType ptr
+*/
+extern void vt_array_slide_reset(struct VitaBaseArrayType *const vbat);
+
+/** Maps a 2d index to 1d index
+    @param row row index
+    @param col col index
+    @param ncols number of columns (horizontal width)
+    @returns size_t 1d index
+*/
+extern size_t vt_index_2d_to_1d(const size_t row, const size_t col, const size_t ncols);
+
+/** Maps a 1d index to 2d index
+    @param row row index to save the value
+    @param col col index to save the value
+    @param idx 1d index to convert to 2d
+    @param ncols number of columns (horizontal width)
+*/
+extern void vt_index_1d_to_2d(size_t *const row, size_t *const col, const size_t idx, const size_t ncols);
+
+#endif // VITA_COMMON_H
