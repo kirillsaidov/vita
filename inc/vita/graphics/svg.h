59c6edef82592b5e7a50fbc58d9370dde5483f52
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-06-01 15:16:04 +0600

    undo func added

--- inc/vita/graphics/svg.h
+++ inc/vita/graphics/svg.h
@@ -16,7 +16,9 @@ typedef struct VitaSVGCanvas {
 
 extern vt_svg_canvas_t *vt_svg_canvas_create(const uint32_t width, const uint32_t height, struct VitaBaseAllocatorType *const alloctr);
 extern void vt_svg_canvas_destroy(vt_svg_canvas_t *svg);
+
 extern void vt_svg_canvas_save(const vt_svg_canvas_t *const svg, const char *const filename);
+extern void vt_svg_canvas_undo(const vt_svg_canvas_t *const svg);
 
 extern void vt_svg_canvas_draw_line(vt_svg_canvas_t *const svg, const int32_t x1, const int32_t y1, const int32_t x2, const int32_t y2, const uint32_t stroke_width, const char *const stroke_color);
 extern void vt_svg_canvas_draw_circle(vt_svg_canvas_t *const svg, const int32_t x, const int32_t y, const uint32_t r, const uint32_t stroke_width, const char *const stroke_color, const char *const fill_color);
