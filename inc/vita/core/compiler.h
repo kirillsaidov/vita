728574a8ec695cb655c847e6e0351be3f99e7308
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-06-29 19:53:32 +0600

    compiler vendor name added

--- inc/vita/core/compiler.h
+++ inc/vita/core/compiler.h
@@ -8,17 +8,20 @@
 #include "version.h"
 
 #if defined(__clang__)
-    #define VT_COMPILER_NAME "CLANG"
+    #define VT_COMPILER_NAME "clang"
+    #define VT_COMPILER_VENDOR "LLVM Clang"
     #define VT_COMPILER_VERSION_MAJOR __clang_major__
     #define VT_COMPILER_VERSION_MINOR __clang_minor__
     #define VT_COMPILER_VERSION_PATCH __clang_patchlevel__
 #elif defined(__GNUC__) || defined(__GNUG__)
-    #define VT_COMPILER_NAME "GCC"
+    #define VT_COMPILER_NAME "gcc"
+    #define VT_COMPILER_VENDOR "GCC"
     #define VT_COMPILER_VERSION_MAJOR __GNUC__
     #define VT_COMPILER_VERSION_MINOR __GNUC_MINOR__
     #define VT_COMPILER_VERSION_PATCH __GNUC_PATCHLEVEL__
 #else
     #define VT_COMPILER_NAME "UNKNOWN"
+    #define VT_COMPILER_VENDOR "UNKNOWN"
     #define VT_COMPILER_VERSION_MAJOR 0
     #define VT_COMPILER_VERSION_MINOR 0
     #define VT_COMPILER_VERSION_PATCH 0
--- tests/src/test_compiler.c
+++ tests/src/test_compiler.c
@@ -10,7 +10,7 @@ int32_t main(void) {
     #elif defined(__linux__)
         assert(vt_str_equals(v.str, "11.3.0"));
     #else
-        //...
+        assert(vt_str_equals(v.str, "13.0.0"));
     #endif
     
     return 0;
