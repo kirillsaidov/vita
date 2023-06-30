188d6780e9a8e8cfceae6feca5cbe46a2ca98815
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-06-30 12:31:18 +0600

    compiler id added

--- inc/vita/core/compiler.h
+++ inc/vita/core/compiler.h
@@ -7,19 +7,37 @@
 
 #include "version.h"
 
+enum VitaCompilerID {
+    VT_COMPILER_ID_GCC,
+    VT_COMPILER_ID_CLANG,
+    VT_COMPILER_ID_TCC,
+    VT_COMPILER_ID_UNKNOWN,
+    VT_COMPILER_ID_COUNT
+};
+
 #if defined(__clang__)
+    #define VT_COMPILER_ID VT_COMPILER_ID_CLANG
     #define VT_COMPILER_NAME "clang"
     #define VT_COMPILER_VENDOR "LLVM Clang"
     #define VT_COMPILER_VERSION_MAJOR __clang_major__
     #define VT_COMPILER_VERSION_MINOR __clang_minor__
     #define VT_COMPILER_VERSION_PATCH __clang_patchlevel__
 #elif defined(__GNUC__) || defined(__GNUG__)
+    #define VT_COMPILER_ID VT_COMPILER_ID_GCC
     #define VT_COMPILER_NAME "gcc"
     #define VT_COMPILER_VENDOR "GCC"
     #define VT_COMPILER_VERSION_MAJOR __GNUC__
     #define VT_COMPILER_VERSION_MINOR __GNUC_MINOR__
     #define VT_COMPILER_VERSION_PATCH __GNUC_PATCHLEVEL__
+#elif defined(__TINYC__)
+    #define VT_COMPILER_ID VT_COMPILER_ID_TCC
+    #define VT_COMPILER_NAME "tcc"
+    #define VT_COMPILER_VENDOR "TinyCC"
+    #define VT_COMPILER_VERSION_MAJOR TCC_VERSION[0]
+    #define VT_COMPILER_VERSION_MINOR TCC_VERSION[2]
+    #define VT_COMPILER_VERSION_PATCH TCC_VERSION[4]
 #else
+    #define VT_COMPILER_ID VT_COMPILER_ID_UNKNOWN
     #define VT_COMPILER_NAME "UNKNOWN"
     #define VT_COMPILER_VENDOR "UNKNOWN"
     #define VT_COMPILER_VERSION_MAJOR 0
--- tests/src/test_compiler.c
+++ tests/src/test_compiler.c
@@ -7,10 +7,16 @@ int32_t main(void) {
 
     #if defined(_WIN32) || defined(_WIN64)
         assert(vt_str_equals(v.str, "13.1.0"));
+        printf("windows: %s\n", VT_COMPILER_NAME);
+        assert(VT_COMPILER_ID == VT_COMPILER_ID_GCC);
     #elif defined(__linux__)
         assert(vt_str_equals(v.str, "11.3.0"));
+        printf("linux: %s\n", VT_COMPILER_NAME);
+        assert(VT_COMPILER_ID == VT_COMPILER_ID_GCC);
     #else
         assert(vt_str_equals(v.str, "13.0.0"));
+        printf("macos: %s\n", VT_COMPILER_NAME);
+        assert(VT_COMPILER_ID == VT_COMPILER_ID_CLANG);
     #endif
     
     return 0;
