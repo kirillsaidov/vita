b29a9158b64efc094d19118a10aedc1dc62dcfa2
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-07-03 09:23:25 +0600

    CMakeLists update

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ if not exist build (
     md build 
 )
 
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\mingw64\bin\gcc.exe -H%cd% -B%cd%/build -G "MinGW Makefiles"
-cmake --build %cd%/build --config Debug
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\mingw64\bin\gcc.exe -H%cd% -B%cd%/build -G "MinGW Makefiles"
+cmake --build %cd%/build
 
 
 
