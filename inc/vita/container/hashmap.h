d9f16fb7a8746b53b96d970a169f3acf5c49cef1
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-09-22 22:32:58 +0600

    update

---
+++ inc/vita/container/hashmap.h
@@ -0,0 +1,12 @@
+#ifndef VITA_CONTAINER_HASHMAP_H
+#define VITA_CONTAINER_HASHMAP_H
+
+/** HASHMAP MODULE
+    - 
+*/
+
+#include "common.h"
+
+
+#endif // VITA_CONTAINER_HASHMAP_H
+
---
+++ src/vita/container/hashmap.c
@@ -0,0 +1,4 @@
+#include "vita/container/hashmap.h"
+
+// ...
+
