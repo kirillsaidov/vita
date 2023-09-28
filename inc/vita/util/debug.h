adc2e407b2d6b60ecd5995996d84720d037aabe1
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-09-28 09:02:37 +0600

    vita/util/debug.h comments update

--- inc/vita/util/debug.h
+++ inc/vita/util/debug.h
@@ -8,6 +8,10 @@
     - VT_DEBUG_PRINTF
     - VT_CHECK
     - VT_ENFORCE
+    - VT_NOTE
+    - VT_REQUIRED
+    - VT_TODO
+    - VT_UNIMPLEMENTED
 
  * Functions
     - vt_debug_assert
@@ -37,10 +41,10 @@
 // the same as assert, but isn't removed in release builds
 #define VT_ENFORCE(expr, ...) vt_debug_assert(expr, VT_STRING_OF(expr), "ENFORCE FAILURE", true,  __SOURCE_FILENAME__, __func__, __LINE__, __VA_ARGS__)
 
-// leave a note
+// leave a note, move on
 #define VT_NOTE(...) vt_debug_assert(false, "!", "NOTE", false, __SOURCE_FILENAME__, __func__, __LINE__, __VA_ARGS__)
 
-// leave important information
+// output important information, move on
 #define VT_REQUIRED(...) vt_debug_assert(false, "!", "REQUIRED", false, __SOURCE_FILENAME__, __func__, __LINE__, __VA_ARGS__)
 
 // output msg, then quit
