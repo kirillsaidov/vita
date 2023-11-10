63d11effc8ddd9117ef7b9f4fd175e15b9e64e14
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-11-10 11:23:47 +0600

    updatE

--- tests/src/test_sockets_server.c
+++ tests/src/test_sockets_server.c
@@ -64,29 +64,35 @@ void server_with_timeout(void) {
 
         // loop: only receive messages
         while (true) {
+            /**
+             * LISTEN
+             */
+            // VT_LOG_INFO("Waiting for client...");
+            // const vt_socket_t client_id = vt_socket_accept_client(server_id);
+            // assert(client_id >= 0);
+
+            // VT_LOG_INFO("Client connected. Reading data...");
+            // static char buffer[VT_STR_TMP_BUFFER_SIZE] = {0};
+            // const int32_t size_received = vt_socket_receive_timed(client_id, buffer, sizeof(buffer), 5000);
+            // assert(size_received >= 0);
+
+            // if (size_received > 0) VT_LOG_WARN("Data received: [%s] | bytes: %zu", buffer, size_received);
+            // else VT_LOG_WARN("Nothing received. Listening...");
+
+            /**
+             * SEND
+             */
             VT_LOG_INFO("Waiting for client...");
             const vt_socket_t client_id = vt_socket_accept_client(server_id);
             assert(client_id >= 0);
 
-            VT_LOG_INFO("Client connected. Reading data...");
-            static char buffer[VT_STR_TMP_BUFFER_SIZE] = {0};
-            const int32_t size_received = vt_socket_receive_timed(client_id, buffer, sizeof(buffer), 5000);
-            assert(size_received >= 0);
+            VT_LOG_INFO("Client connected. Sending data...");
+            char buffer[4096] = {0}; vt_file_read_to_buffer("/Users/krillos/MyFiles/dev/repos/git.kirillsaidov/zme/assets/order_example.json", buffer, sizeof(buffer));
+            const int32_t size_sent = vt_socket_send(client_id, buffer, strlen(buffer));
+            assert(size_sent >= 0);
 
-            if (size_received > 0) VT_LOG_WARN("Data received: [%s] | bytes: %zu", buffer, size_received);
-            else VT_LOG_WARN("Nothing received. Listening...");
-            // int32_t ret = user_get_msg(buffer, sizeof(buffer));
-            // if (ret == -1) break;
-            // else if (ret == 0) continue;
-
-            // const size_t msg_len = strlen(buffer) + 1;
-            // VT_LOG_INFO("Message entered: [%s] | bytes: %zu", buffer, msg_len);
-
-            // VT_LOG_INFO("Sending message...");
-            // const int32_t size_sent = vt_socket_send(client_id, buffer, msg_len);
-            // assert(size_sent == msg_len);
-
-            // VT_LOG_INFO("Done.");
+            if (size_sent > 0) VT_LOG_WARN("Data sent: [%s] | bytes: %zu", buffer, size_sent);
+            else VT_LOG_WARN("Failed to send a message. Listening again...");
         }
 
         VT_LOG_INFO("Closing the socket...");
